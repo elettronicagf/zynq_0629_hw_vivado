098537ac205db2511f3ff34685b527da6d09b88f
Stefano Donati <stefano.donati@elettronicagf.it>
2019-01-25 17:02:39 +0100

    Scripts improvements

--- createProject.sh
+++ createProject.sh
@@ -1,9 +1,11 @@
+set -x
 project_name=minized_foundation
+project_home=$PWD
 path_tcl=$(dirname $(readlink -f $(find | grep minized_petalinux_collaudo.tcl)))
 
 cd $path_tcl
 
-vivado -mode batch -source minized_petalinux_collaudo.tcl
+vivado -mode batch -source minized_petalinux_collaudo.tcl -tclargs --origin_dir $project_home
 
 cp -r $project_name/* .
 rm -rf $project_name
