f8317168ea4efe27d68241ab9ffab589fd9d416f
Stefano Donati <stefano.donati@elettronicagf.it>
2019-02-06 11:11:40 +0100

    Renamed minized_petalinux_collaudo.tcl to 3sm7_petalinux.tcl

--- exportProject.tcl
+++ exportProject.tcl
@@ -1,4 +1,4 @@
 open_project $env(PWD)/Projects/minized_foundation/MINIZED/minized_foundation.xpr
-write_project_tcl -paths_relative_to $env(PWD) $env(PWD)/Projects/minized_foundation/MINIZED/minized_petalinux_collaudo.tcl -force
+write_project_tcl -paths_relative_to $env(PWD) $env(PWD)/Projects/minized_foundation/MINIZED/3sm7_petalinux.tcl -force
 exit
 
