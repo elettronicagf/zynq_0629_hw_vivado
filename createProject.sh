2510268c0968e454453f9946c0624ce7ebbf43dc
Stefano Donati <stefano.donati@elettronicagf.it>
2019-02-06 11:11:40 +0100

    Renamed minized_petalinux_collaudo.tcl to 3sm7_petalinux.tcl

--- createProject.sh
+++ createProject.sh
@@ -1,15 +1,12 @@
 set -x
 project_name=minized_foundation
 project_home=$PWD
-path_tcl=$(dirname $(readlink -f $(find | grep minized_petalinux_collaudo.tcl)))
+path_tcl=$(dirname $(readlink -f $(find | grep 3sm7_petalinux.tcl)))
 
 cd $path_tcl
 
-vivado -mode batch -source minized_petalinux_collaudo.tcl -tclargs --origin_dir $project_home
+vivado -mode batch -source 3sm7_petalinux.tcl -tclargs --origin_dir $project_home
 
 cp -r $project_name/* .
 rm -rf $project_name
 sed -i -e 's=imports=bd/minized_foundation=g' $project_name.xpr
-
-#      <File Path="$PSRCDIR/sources_1/imports             /hdl/minized_foundation_wrapper.vhd">
-#      <File Path="$PSRCDIR/sources_1/bd/minized_petalinux/hdl/minized_foundation_wrapper.vhd">
